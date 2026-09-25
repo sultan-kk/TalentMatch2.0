e472558b603062a5427cb798ab0a63676235b680
sultan-kk <102281043+sultan-kk@users.noreply.github.com>
2026-09-25 17:15:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
-st.sidebar.image("logo.jpg")
+st.sidebar.image("logo.png")
 CUSTOM_CSS = """
 <style>
 html, body, [class*="css"] {
