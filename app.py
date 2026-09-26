2c2003bfbf8a7da2c4feacbc6c8ea36cf5aca84d
sultan-kk <102281043+sultan-kk@users.noreply.github.com>
2026-09-26 20:21:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ AUTH_DB_FILE = "hr_users.db"
 
 MEEZAN_TITLE = "Muhammad Sultan Sheraz"
 MEEZAN_IBAN = "PK24MEZN0098820105114718"
-
+SADAPAY_NUMBER = "0325-8641257"
 def init_auth_db():
     conn = sqlite3.connect(AUTH_DB_FILE)
     cursor = conn.cursor()
